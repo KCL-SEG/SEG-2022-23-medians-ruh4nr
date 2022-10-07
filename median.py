9acbec61cb8c307a37b91f41a27d3523223d2178
Ruhan Rahman <k20015337@kcl.ac.uk>
2022-10-07 19:48:20 +0100

    Finding median

--- median.py
+++ median.py
@@ -1,6 +1,15 @@
 """Median calculator."""
 """ENTER YOUR SOLUTION HERE!"""
 
+def findMedian(numbers):
+    length = len(numbers)
+    index = (length-1) // 2
+
+    if (length % 2):
+        return numbers[index]
+    else:
+        return (numbers[index] + numbers[index + 1])/2.0
+
 while True:
     try:
         print("Enter a list of numbers separated by commas: ")
@@ -10,3 +19,4 @@ while True:
     else:
         break
 print(numbers)
+print("The median is " + str(findMedian(numbers)))
